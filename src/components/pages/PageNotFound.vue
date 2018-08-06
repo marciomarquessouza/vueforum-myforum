f17c23788b06e04e8f6f9ed282846868e61c8ed9
marciomarquessouza <marciomultimedia@gmail.com>
2018-08-06 00:11:20 -0300

    addes page not found

---
+++ src/components/pages/PageNotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-full">
+    <h1>Page Not Found</h1>
+    <p>Sorry, we coudn't find out your content. Got to
+      <router-link :to="{name:'PageHome'}">
+        Home
+      </router-link>
+      my friend.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 100px;
+  }
+  p {
+    font-size: 50px;
+    font-weight: 100px;
+  }
+  div {
+    text-align: center;
+  }
+</style>
