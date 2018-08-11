9feaaf04e843ba6829f0e1af2eff0a7a4e37ed6b
marciomarquessouza <marciomultimedia@gmail.com>
2018-08-11 18:56:59 -0300

    added filter and includes

---
+++ src/components/PostListItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="post">
+      <div class="user-info">
+          <a href=# class="user-name">{{ user.name }}</a>
+          <a href="profile.html#profile-details">
+              <img class="avatar-large"
+              :src="user.avatar" alt="">
+          </a>
+          <p class="desktop-only text-small">{{ userPosts }} posts</p>
+          <span class="online desktop-only">online</span>
+      </div>
+
+      <div class="post-content">
+          <div>
+            <p>
+              {{ post.text }}
+            </p>
+          </div>
+      </div>
+
+      <div class="post-date text-faded">
+          {{ post.publishedAt }}
+      </div>
+  </div>
+</template>
+
+<script>
+
+import sourceData from '@/data.json';
+
+export default {
+  props: {
+    post: {
+      required: true,
+      type: Object,
+    },
+  },
+  computed: {
+    user() {
+      return sourceData.users[this.post.userId];
+    },
+    userPosts() {
+      return Object.keys(this.user.posts).length;
+    },
+  },
+};
+
+</script>
--- src/components/pages/PageThreadShow.vue
+++ src/components/pages/PageThreadShow.vue
@@ -1,42 +1,9 @@
 <template>
   <div>
     <div class="col-large push-top">
-
         <h1>{{ thread.title }}</h1>
+        <PostList :posts="posts" />
 
-        <div class="post-list">
-
-            <div v-for="postId in thread.posts"  :key="postId.key" class="post">
-
-                <div class="user-info">
-                    <a href=# class="user-name">{{ users[posts[postId].userId].name }}</a>
-
-                    <a href="profile.html#profile-details">
-                        <img class="avatar-large"
-                        :src="users[posts[postId].userId].avatar" alt="">
-                    </a>
-
-                    <p class="desktop-only text-small">107 posts</p>
-
-                    <span class="online desktop-only">online</span>
-
-                </div>
-
-                <div class="post-content">
-                    <div>
-                      <p>
-                        {{ posts[postId].text }}
-                      </p>
-                    </div>
-                </div>
-
-                <div class="post-date text-faded">
-                    {{ posts[postId].publishedAt }}
-                </div>
-
-            </div>
-
-        </div>
     </div>
   </div>
 </template>
@@ -44,8 +11,12 @@
 <script>
 
 import sourceData from '@/data.json';
+import PostList from '@/components/PostList';
 
 export default {
+  components: {
+    PostList,
+  },
   props: {
     id: {
       required: true,
@@ -55,9 +26,14 @@ export default {
   data() {
     return {
       thread: sourceData.threads[this.id],
-      posts: sourceData.posts,
-      users: sourceData.users,
     };
+  },
+  computed: {
+    posts() {
+      const postIds = Object.keys(this.thread.posts);
+      return Object.values(sourceData.posts)
+        .filter(post => postIds.includes(post['.key']));
+    },
   },
 };
 </script>
