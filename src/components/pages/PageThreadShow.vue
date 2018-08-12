d3f7030aae484ecb33ad44c135d4787f0ec14ac3
marciomarquessouza <marciomultimedia@gmail.com>
2018-08-11 22:19:26 -0300

    addes post logic

--- src/components/pages/PageThreadShow.vue
+++ src/components/pages/PageThreadShow.vue
@@ -3,7 +3,18 @@
     <div class="col-large push-top">
         <h1>{{ thread.title }}</h1>
         <PostList :posts="posts" />
-
+        <form @submit.prevent="addPost">
+          <div class="form-group">
+            <textarea rows="10"
+                      cols="30"
+                      class="form-input"
+                      v-model="initialTextArea"
+                      ></textarea>
+          </div>
+          <div class="form-actions">
+            <button class="btn-blue">Submit Post</button>
+          </div>
+        </form>
     </div>
   </div>
 </template>
@@ -26,7 +37,23 @@ export default {
   data() {
     return {
       thread: sourceData.threads[this.id],
+      initialTextArea: '',
     };
+  },
+  methods: {
+    addPost() {
+      const postId = `great-post ${Math.random()}`;
+      const post = {
+        publishedAt: Math.floor(Date.now() / 1000),
+        text: this.initialTextArea,
+        threadId: this.id,
+        userId: 'Miej9zSGMRZKDvMXzfxjVOyv3RF3',
+        '.key': postId,
+      };
+      this.$set(sourceData.posts, postId, post);
+      this.$set(this.thread.posts, postId, postId);
+      this.$set(sourceData.users[post.userId].posts, postId, postId);
+    },
   },
   computed: {
     posts() {
