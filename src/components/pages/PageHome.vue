ab0be5a4789060304329e0acd9c1ae574c75a55b
marciomarquessouza <marciomultimedia@gmail.com>
2018-08-18 14:39:54 -0300

    Forum components

--- src/components/pages/PageHome.vue
+++ src/components/pages/PageHome.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="col-full">
     <h1>Ana Maria Nerd</h1>
-    <ThreadList :threads="threads" />
+    <ForumList :forums="forums"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json';
-import ThreadList from '@/components/ThreadList';
+import ForumList from '@/components/ForumList';
 
 export default {
   components: {
-    ThreadList,
+    ForumList,
   },
   data() {
     return {
-      threads: Object.values(sourceData.threads),
+      forums: Object.values(sourceData.forums),
       posts: sourceData.posts,
       users: sourceData.users,
     };
